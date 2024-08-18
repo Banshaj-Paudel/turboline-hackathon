113b81163a222506e87969870a74b647ee8f7b86
Banshaj <bhairutribrid@gmail.com>
2024-08-18 19:51:44 +0545

    Final modifications

--- templates/third_site/server/get_twowheel.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('connection.php');
-$stmt = $conn->prepare("SELECT * FROM products_vehicles WHERE vehicle_category='Bike' LIMIT 4");
-
-$stmt->execute();
-
-$twowheel = $stmt->get_result();
-?>
